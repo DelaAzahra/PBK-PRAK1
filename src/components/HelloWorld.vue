20f77a36c44449f99b090e9867cdd479730eb883
DelaAzahra <delaazahra@student.uir.ac.id>
2025-05-23 21:01:03 +0700

    Perubahan pada file helloworld dengan menambahkan biodata diri

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,8 +14,16 @@ const count = ref(0)
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+      BIODATA DIRI
+    </p>
+    <p>
+      Nama : Dela Azahra
+    </p>
+    <p>
+      Npm : 233510200
+    </p>
+    <p>
+      Kelas : 4D
     </p>
   </div>
 
